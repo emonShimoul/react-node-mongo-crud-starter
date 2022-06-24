f40b4de2a5b503877f03a68695c0c78dae437243
emonShimoul <emonshimoul@gmail.com>
2022-06-25 00:30:00 +0600

    fetch from the api using mongo

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    }, [])
     return (
         <div>
-            <h2>This is Users</h2>
+            <h2>Users: {users.length}</h2>
         </div>
     );
 };
