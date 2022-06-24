09a6898560af02801879aad4877dfcafd4f6db51
emonShimoul <emonshimoul@gmail.com>
2022-06-25 01:19:21 +0600

    load single item by id and display the user info

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const [user, setUser] = useState({});
     const {id} = useParams();
+
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUser(data));
+    }, [])
+
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update {user.name}</h2>
             <p><small>{id}</small></p>
         </div>
     );
