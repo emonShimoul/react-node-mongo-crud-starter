6b08ea027c2b1294bfc46bbd172fb926444fbc2b
emonShimoul <emonshimoul@gmail.com>
2022-06-25 01:06:29 +0600

    get the id of the specific user

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const {id} = useParams();
     return (
         <div>
             <h2>This is Update User</h2>
+            <p><small>{id}</small></p>
         </div>
     );
 };
